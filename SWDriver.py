49b2d1e41f435ee803cc00042a025367a9cacbc9
UxuginPython <84234447+UxuginPython@users.noreply.github.com>
2022-06-01 16:58:13 -0600

    Update SWDriver.py

--- SWDriver.py
+++ SWDriver.py
@@ -1,7 +1,12 @@
+from random import randrange
 temperature=70
 wind_speed=10
 pressure=50
 wind_direction=0
 connected=True
 def update():
-    pass
+    global temperature, wind_speed, pressure, wind_direction
+    #temperature=randrange(0, 100)
+    #wind_speed=randrange(0, 100)
+    #pressure=randrange(0, 100)
+    wind_direction+=1
