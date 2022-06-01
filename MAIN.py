a5cc97136709df89d687ee63a1a0f57fa8810113
UxuginPython <84234447+UxuginPython@users.noreply.github.com>
2022-06-01 16:57:47 -0600

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -1,6 +1,17 @@
+try:
+    from tkinter import *
+except:
+    from warnings import warn
+    warn('This software is designed for Python 3. However, Python 2 was detected.')
+    from Tkinter import *
 from WeatherUI import WeatherUI
 import SWDriver
-wui=WeatherUI(driver=SWDriver)
+window=Tk()
+window.title('Weather')
+ui=WeatherUI(window, driver=SWDriver)
+ui.pack()
 while True:
     SWDriver.update()
-    wui.update()
+    ui.update()
+    window.update()
+    window.update_idletasks()
